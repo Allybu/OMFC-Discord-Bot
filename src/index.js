6d056097da9f41874ea25df7b50de8c773fe7801
Oliver <register@omfc.de>
2021-03-25 14:10:27 +0100

    Deploy test

--- src/index.js
+++ src/index.js
@@ -35,7 +35,7 @@ bot.on('ready', () => {
     if (env === 'dev') {
         bot.user.setActivity('Under construction...');
     } else if (env === 'prod') {
-        bot.user.setActivity('Diablo IV');
+        bot.user.setActivity('Diablo V');
     }
     listenForInviteReactions(bot);
 });
